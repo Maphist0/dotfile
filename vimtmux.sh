92788baa5ba5a719211de59066ee13cc25c7077a
Maphist0 <28743569+Maphist0@users.noreply.github.com>
2019-03-02 21:00:06 +0800

    Add vimtmux

--- vimtmux.sh
+++ vimtmux.sh
@@ -7,4 +7,8 @@ echo Creating session with name: $sessname
 
 tmux new-session -d -n $sessname
 tmux rename-session $sessname
+tmux send-keys -t $sessname 'vim' C-m
+tmux split-window -h 
+tmux split-window -v -p 30
+tmux select-pane -t 0
 tmux attach-session
